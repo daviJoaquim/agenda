23ee44daf931d9bd5c8ff2a2b56b4e1b49a5502e
daviJoaquim <davijoaquimamerico@gmail.com>
2026-02-02 16:39:00 -0300

    Agenda

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def agenda():
         titulo_tarefa = request.form['titulo-tarefa']
         data_conclusao = request.form['data-conclusao']
         tarefa = Tarefa(titulo_tarefa, data_conclusao)
+        tarefa.salvar_tarefa()
 
     return render_template('agenda.html', titulo='Agenda', tarefa=tarefa)
 
