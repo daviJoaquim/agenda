62a2db671ac10ff4755d36370c4ad12a6bdd484b
Davi Joaquim <davijoaquimamerico@gmail.com>
2026-02-20 00:46:55 +0000

    Agenda atualizada

--- models/database.py
+++ models/database.py
@@ -1,63 +1,67 @@
 from sqlite3 import Connection, connect, Cursor
-from types import TracebackType
 from typing import Any, Optional, Self, Type
+from types import TracebackType
 from dotenv import load_dotenv
 import traceback
 import os
 
-load_dotenv() # Procura um arquivo .env com variáveis
+load_dotenv() # Procura um arquivo .env com variáveis 
 DB_PATH = os.getenv('DATABASE', './data/tarefas.sqlite3')
 
-def init_db(db_name: str = DB_PATH) -> None:
+def init_db(db_name: str = DB_PATH):
     with connect(db_name) as conn:
         conn.execute("""
         CREATE TABLE IF NOT EXISTS tarefas (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             titulo_tarefa TEXT NOT NULL,
-            data_conclusao TEXT
+            data_conclusao TEXT      
         );
         """)
 
-class Database: 
+class Database:
     """
-        Classe que gerencia conexões e oprerações com um banco de dados SQLite. Utiliza o protocolo de gerenciamento de contexto para garantit que a conexão seja encerrada corretamente.
+        Classe que regencia conexões e operações com o banco de dados SQLitre. Utiliza o protocolo de gerenciamento de contxtos para garantir qua a conexão seja encerrada corretamente
     """
-    def __init__(self, db_name: str = DB_PATH) -> None :
+    def __init__(self, db_name: str = DB_PATH) -> None:
         self.connection: Connection = connect(db_name)
         self.cursor: Cursor = self.connection.cursor()
-        self.executar('''
-            CREATE TABLE IF NOT EXISTS tarefas (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                titulo_tarefa TEXT NOT NULL,
-                data_conclusao TEXT);
-                ''')
+        self.executar("""
+        CREATE TABLE IF NOT EXISTS tarefas (
+         id INTEGER PRIMARY KEY AUTOINCREMENT,
+         titulo_tarefa TEXT NOT NULL,
+         data_conclusao TEXT);
+        """)
 
     def executar(self, query: str, params: tuple = ()) -> Cursor:
         self.cursor.execute(query, params)
         self.connection.commit()
         return self.cursor
-    
+
     def buscar_tudo(self, query: str, params: tuple = ()) -> list[Any]:
         self.cursor.execute(query, params)
         return self.cursor.fetchall()
-    
+
     def close(self) -> None:
         self.connection.close()
 
+    # Métodos para o gerenciamento de contexto
+
+    # Método de entrada do contexto
     def __enter__(self) -> Self:
         return self
 
+    # Método de saída do contexto
     def __exit__(
-            self, 
-            exc_type: Optional[Type[BaseException]], 
-            exc_value: Optional[BaseException], 
-            tb: Optional[TracebackType]) -> None:
-
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        tb: Optional[TracebackType],
+    ) -> None:
         if exc_type is not None:
-            print('Execeção capturada no contexto:')
-            print(f'Tipo: {exc_type.__name__}')
-            print(f'Mensagem: {exc_value}')
-            print('Traceback completo:')
+            print("Exceção capiturar no contexto: ")
+            print(f"Tipo: {exc_type.__name__}")
+            print(f"Mensagem: {exc_value}")
+            print("Traceback completo:")
             traceback.print_tb(tb)
 
         self.close()
--- models/tarefa.py
+++ models/tarefa.py
@@ -1,53 +1,62 @@
-from models.database import Database
-from typing import Self, Any, Optional
-from sqlite3 import Cursor
 
-class Tarefa: 
+from sqlite3 import Cursor
+from typing import Optional, Self, Any
+from models.database import Database
+
+class Tarefa:
     """
-    Classe para representar uma tarefa, com métodos para salvar, obter, excluir e atualizar tarefas em um banco de dados usando a classe `Database`.
+        Classe Para representar tarefa, com metodos para salvar, obter, excluir e  atualizar tarefas com um banco de dados usando a classe Database.
     """
-    def __init__(self: Self, titulo_tarefa: Optional[str], data_conclusao: Optional[str] = None, id_tarefa: Optional[int] = None) -> None:
+    def __init__(self: Self, titulo_tarefa: Optional[str], data_conclusao: Optional[str] = None, id_tarefa:Optional[int] = None,)-> None:
         self.titulo_tarefa: Optional[str] = titulo_tarefa
-        self.data_conclusao: Optional[str] = data_conclusao
+        self.data_conclusao: Optional[str]  = data_conclusao
         self.id_tarefa: Optional[int] = id_tarefa
 
     @classmethod
     def id(cls, id: int) -> Self:
-     with Database() as db:
-        query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas WHERE id = ?;'
-        params: tuple = (id,)
-        resultado: list[Any] = db.buscar_tudo(query, params)
-        #Desempacotamento de coleção
-        [[titulo, data]] = resultado
+        with Database() as db:
+            query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas WHERE id = ?;'
+            params: tuple = (id,)
+            resultado = db.buscar_tudo(query, params)
+            print(resultado)
+
+            #desenpacotamento de coleção
+            [[titulo, data]] = resultado
 
         return cls(id_tarefa=id, titulo_tarefa=titulo, data_conclusao=data)
-     
-
-    def salvar_tarefa(self: Self) -> None:
+        
+    def salvar_tarefa(self: Self)-> None:
         with Database() as db:
-            query: str ='INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);'
+            query: str = " INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);"
             params: tuple = (self.titulo_tarefa, self.data_conclusao)
             db.executar(query, params)
-    
+
     @classmethod
     def obter_tarefas(cls) -> list[Self]:
         with Database() as db:
             query: str = 'SELECT titulo_tarefa, data_conclusao, id FROM tarefas;'
-            resultados:list[Any] = db.buscar_tudo(query)
-            tarefas: list[Self] = [cls(titulo, data, id) for titulo, data, id in resultados]
+            resultados: list[Any] = db.buscar_tudo(query)
+            tarefas: list[Any] = [cls(titulo, data, id) for titulo, data, id in resultados]
             return tarefas
-
-                        
+    
     def excluir_tarefa(self) -> Cursor:
         with Database() as db:
             query: str = 'DELETE FROM tarefas WHERE id = ?;'
             params: tuple = (self.id_tarefa,)
             resultado: Cursor = db.executar(query, params)
             return resultado
-        
-    def atualizar_tarefa(self) -> Cursor:
+    
+    def atualizar_tarefas(self) -> Cursor:
+           with Database() as db:
+            query: str = 'UPDATE tarefas SET titulo_tarefa = ?, data_conclusao = ? WHERE id = ?;'
+            params: tuple = (self.titulo_tarefa, self.data_conclusao, self.id_tarefa)
+            resultado: Cursor = db.executar(query, params)
+            return resultado
+           
+    def concluir_tarefa(self) -> Cursor:
         with Database() as db:
-            query: str = 'UPDATE tarefas SET titulo_tarefa = ?, data_conclusao = ?  WHERE id = ? ;'
-            params: tuple = (self.titulo_tarefa,self.data_conclusao,self.id_tarefa)
-            resultados: Cursor = db.executar(query, params)
-            return resultados
+            query: str = 'UPDATE tarefas SET titulo_tarefa = ?, data_conclusao = ? WHERE id = ?;'
+            params: tuple = (self.titulo_tarefa, self.data_conclusao, self.id_tarefa)
+            resultado: Cursor = db.executar(query, params)
+            return resultado
+    
